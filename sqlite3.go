550ae00dbb967647e7614eb2fc0c37c60d0aa6cc
Lorenzo Bolla <lbolla@gmail.com>
2012-12-18 11:12:06 +0000

    Added missing files

---
+++ sqlite3.go
@@ -0,0 +1,103 @@
+package hood
+
+import (
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"reflect"
+	"time"
+	"unsafe"
+)
+
+func init() {
+	RegisterDialect("sqlite3", &Sqlite{})
+}
+
+type Sqlite struct{}
+
+func (d *Sqlite) Marker(pos int) string {
+//         return "?"
+	return fmt.Sprintf("$%d", pos+1)
+}
+
+func (d *Sqlite) SqlType(f interface{}, size int) string {
+	switch f.(type) {
+	case Id:
+		return "integer"
+	case VarChar:
+		return "text"
+	case time.Time:
+		return "text"
+	case bool:
+		// 0 or 1
+		return "integer"
+	case int, int8, int16, int32, uint, uint8, uint16, uint32, int64, uint64:
+		return "integer"
+	case float32, float64:
+		return "real"
+	case []byte:
+		return "text"
+	case string:
+		return "text"
+	}
+	panic("invalid sql type")
+}
+
+func (d *Sqlite) ValueToField(value reflect.Value, field reflect.Value) error {
+	switch field.Type().Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		field.SetInt(value.Elem().Int())
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		// reading uint from int value causes panic
+		switch value.Elem().Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			field.SetUint(uint64(value.Elem().Int()))
+		default:
+			field.SetUint(value.Elem().Uint())
+		}
+	case reflect.Bool:
+		if value.Elem().Int() == 0 {
+			field.SetBool(false)
+		} else {
+			field.SetBool(true)
+		}
+	case reflect.Float32, reflect.Float64:
+		field.SetFloat(value.Elem().Float())
+	case reflect.String:
+		field.SetString(value.Elem().String())
+	case reflect.Slice:
+		if reflect.TypeOf(value.Interface()).Elem().Kind() == reflect.Uint8 {
+			field.SetBytes(value.Elem().Bytes())
+		}
+	case reflect.Struct:
+		if field.Type() == reflect.TypeOf(time.Time{}) {
+			// TODO
+			t, err := time.Parse("2006-01-02 15:04:05", value.Elem().String())
+			if err != nil {
+				return err
+			}
+			v := reflect.NewAt(reflect.TypeOf(time.Time{}), unsafe.Pointer(&t))
+			field.Set(v.Elem())
+		}
+	}
+	return nil
+}
+
+func (d *Sqlite) Insert(hood *Hood, model *Model, query string, args ...interface{}) (Id, error, bool) {
+	return -1, nil, false
+}
+
+func (d *Sqlite) StmtNotNull() string {
+	return "NOT NULL"
+}
+
+func (d *Sqlite) StmtDefault(s string) string {
+	return fmt.Sprintf("DEFAULT %v", s)
+}
+
+func (d *Sqlite) StmtPrimaryKey() string {
+	return "PRIMARY KEY"
+}
+
+func (d *Sqlite) StmtAutoIncrement() string {
+	return "AUTOINCREMENT"
+}
