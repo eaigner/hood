2728429f0b222109e3f3af5a50d6b2abb4f5cf6b
Erik Aigner <erik@chocomoko.com>
2013-01-07 02:26:00 +0100

    removed fmt

--- cmd/templates/migration.go
+++ cmd/templates/migration.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"github.com/eaigner/hood"
-	"fmt"
 )
 
 func (m *M) {{.Name}}_{{.Timestamp}}_Up(hood *hood.Hood) {
 	// TODO: implement
-	fmt.Println("up")
 }
 
 func (m *M) {{.Name}}_{{.Timestamp}}_Down(hood *hood.Hood) {
 	// TODO: implement
-	fmt.Println("down")
 }
