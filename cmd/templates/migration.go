0c1fbb4e889df632fa025a445f5e7fdf07a8fa07
Erik Aigner <erik@chocomoko.com>
2013-01-10 01:40:01 +0100

    change parameter name not to interfere with import

--- cmd/templates/migration.go
+++ cmd/templates/migration.go
@@ -4,10 +4,10 @@ import (
 	"github.com/eaigner/hood"
 )
 
-func (m *M) {{.Name}}_{{.Timestamp}}_Up(hood *hood.Hood) {
+func (m *M) {{.Name}}_{{.Timestamp}}_Up(hd *hood.Hood) {
 	// TODO: implement
 }
 
-func (m *M) {{.Name}}_{{.Timestamp}}_Down(hood *hood.Hood) {
+func (m *M) {{.Name}}_{{.Timestamp}}_Down(hd *hood.Hood) {
 	// TODO: implement
 }
